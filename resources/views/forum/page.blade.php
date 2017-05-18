494799b41bfe4282a421f2459b04a2283fb582d4
IK <ik@develup.pro>
2017-05-18 19:04:35 +0300

    upd

--- resources/views/forum/page.blade.php
+++ resources/views/forum/page.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <header>
-        <div class="page-title">
-            <div class="grid-row">
-                <h1>Page 404</h1>
-                <nav class="bread-crumb">
-                    <a href="index.html">Home</a>
-                    <i class="fa fa-long-arrow-right"></i>
-                    <a href="content-elements.html">Features</a>
-                    <i class="fa fa-long-arrow-right"></i>
+        <div class="page-title">ow-right"></i>
                     <a href="">Pages</a>
+                    <div class="grid-row">
+                        <h1>Page 404</h1>
+                        <nav class="bread-crumb">
+                            <a href="index.html">Home</a>
+                            <i class="fa fa-long-arrow-right"></i>
+                            <a href="content-elements.html">Features</a>
+                            <i class="fa fa-long-arr
                     <i class="fa fa-long-arrow-right"></i>
                     <a href="">Page 404</a>
                 </nav>
